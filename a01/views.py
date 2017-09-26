4497c8f38a47c083525c43e65e8ec986e055f3fb
tamir-ganbat <31709476+tamir-ganbat@users.noreply.github.com>
2017-09-26 15:53:07 -0500

    Movie page Alpha

--- a01/views.py
+++ a01/views.py
@@ -72,7 +72,7 @@ class ListView(View):
     get_class = None
 
     def get(self, *args):
-        obj_list = self.get_class.objects.all()
+        obj_list = self.get_class.objects.all()[:3]
         json_data = serializers.serialize('json', obj_list)
 
         return HttpResponse(json_data, content_type='json')
